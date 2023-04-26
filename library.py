f852cc021bb81009e8bd1245139d7300bbb03348
swetha253 <130964034+swetha253@users.noreply.github.com>
2023-04-26 20:18:28 +0530

    Create library.py

---
+++ library.py
@@ -0,0 +1,35 @@
+class Library:
+    book = ["The fault in our stars" , "Abang" , "Citadel"]
+    author_book = {
+        "Pablo":["escobar","escobar returns","escobar part 3"]
+    }
+    def getAllBooks():
+        pass
+    def addBook():
+        pass
+    def getBookByAuthorName(authorName):
+        for key in author_book:
+            if key == authorName:
+                print(author_book[key])
+
+
+    def sortBooksInTopologicalOrder(self):
+        book_copy = []
+        for books in self.book:
+            book_copy.append(books)
+
+        return book_copy.sort()
+
+    def getAllSubscribers():
+        pass
+    def getAllEarnings():
+        pass
+    def getUnavailableBooks():
+        pass
+    def mapAuthorWithABook():
+        pass
+class MyLibrary(Library):
+    # it can not have it's own data structure to store book , authors , subscribers , unvailable books
+    def getFaviourtBook():
+        pass
+    def getFaviourtCustomer():
